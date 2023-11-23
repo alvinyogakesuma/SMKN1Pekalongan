dbb10d993b389caee0635a6c563632cbeab71419
alvinyogakesuma <alvinyoga33@gmail.com>
2023-11-24 06:42:23 +0700

    update responsive awal beranda

---
+++ client/news-section.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".news-swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
